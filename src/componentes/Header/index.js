970ad9c0ede8b78c9f45c02d189761006e10b89e
YURI <cg3026507@labA204m36.IFSP>
2024-06-17 10:42:54 -0300

    Inicio

---
+++ src/componentes/Header/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import '/style.css';
+
+const Header = () => {
+  return (
+    <header>
+      <div className='header center'>
+        <picture>
+          <img src='./assets/images/logo.png' alt='Óticas Vida' />
+        </picture>
+        <nav>
+          <ul>
+            <li>
+              <a href='#produtos'>Produtos</a>
+            </li>
+            <li>
+              <a href='#sobre'>Sobre</a>
+            </li>
+            <li>
+              <a href='#contato'>Contato</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
+};
+export default Header;
